cb33d99b954f2f2eddcdedc05ed2a75ebaf00174
Jengopockets <jengopockets@gmail.com>
2019-08-23 12:46:45 -0400

    login and restricted set up

--- auth/auth-model.js
+++ auth/auth-model.js
@@ -1,9 +1,13 @@
 const db = require('../database/dbConfig.js');
 module.exports = {
     add,
+    find,
     findBy,
     findById,
   };
+  function find() {
+    return db('users').select('id', 'username');
+  }
 
 function findBy(filter) {
     return db('users').where(filter);
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-const Users = require('../users/users-model.js');
+const Users = require('./auth-model');
 const secrets = require('../config/secrets.js');
 
 
@@ -23,6 +23,21 @@ router.post('/register', (req, res) => {
 
 router.post('/login', (req, res) => {
   // implement login
+  let { username, password } =req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = getJwt(user);
+        res.status(200).json({message: `Welcome ${user.username}!`, token,});
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
 function getJwt(user) {
